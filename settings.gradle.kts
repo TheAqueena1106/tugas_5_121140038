703c620643b8e0a0bb7a96e32dc320bc8ba1d55d
121140038 <133471669+TheAqueena1106@users.noreply.github.com>
2024-05-24 08:05:01 +0700

    Add files via upload

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "tugas_5_121140038"
+include(":app")
+ 
